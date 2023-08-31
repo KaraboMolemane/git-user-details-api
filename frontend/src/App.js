07c8de95640cf2de2bd32c37797ee3fffcceb79f
KaraboMo <karabo.molemane@adaptit.com>
2023-08-31 19:45:02 +0200

    DataGrid with hardcoded data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import 'react-data-grid/lib/styles.css';
+//import DataGrid from 'react-data-grid';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faBook } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,6 +15,131 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
   const [searchText, setSearchText] = useState('');
+
+  // const columns = [
+  //   { key: 'id', name: 'ID' },
+  //   { key: 'title', name: 'Title' }
+  // ];
+  
+  // const rows = [
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' },
+  //   { id: 0, title: 'Example' },
+  //   { id: 1, title: 'Demo' }
+
+  // ];
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
 
   useEffect(() => {
@@ -52,6 +182,7 @@ function App() {
     );*/
   }
 
+
   return (
     <div className="container my-5">
     <div className="p-5 text-center bg-body-tertiary rounded-3">
@@ -79,7 +210,23 @@ function App() {
       <h4 className='text-uppercase'>{searchText}</h4>
       <span>{results}</span>
       </div>
-
+      {/*<DataGrid columns={columns} rows={rows} />*/}
+      <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
     </div>
 </div>
 );
