bb38e8c6ba423744c66c17e2fc2561b2c349e52b
KaraboMo <karabo.molemane@adaptit.com>
2023-09-05 23:42:37 +0200

    github octokit documentation

--- app.js
+++ app.js
@@ -13,25 +13,6 @@ const API_BASE_URL = "https://api.github.com/";
 // Use Helmet!
 app.use(helmet());
 
-// const octokit = new Octokit({
-//     auth: GIT_USER_API_TOKEN
-//   });
-
-// await octokit.request("GET /octocat", {});
-
-// try {
-//     const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-//       owner: "KaraboMolemane",
-//       repo: "git-user-details-api",
-//       per_page: 2,
-//     });
-
-//     console.log(`Success! Status: ${result.status}. Rate limit remaining: ${result.headers["x-ratelimit-remaining"]}`)
-
-//   } catch (error) {
-//     console.log(`Error! Status: ${error.status}. Rate limit remaining: ${error.headers["x-ratelimit-remaining"]}. Message: ${error.response.data.message}`)
-//   }
-
 app.get("/", (req, res) => {
   res.send(
     "Hello world! Our app is now wearing a Helmet for security reasons."
@@ -155,6 +136,7 @@ app.get("*", function (req, res, next) {
 
 /*
 USEFUL LINKS
+https://docs.github.com/en/rest/quickstart
 https://stateful.com/blog/github-search-api
 https://youtu.be/7YeOjmpQUnU?si=QOLx_ONFJB40UbRI
 https://youtu.be/EvZ6pjgYA38?si=GFX9OmhZHRAAcvP_
