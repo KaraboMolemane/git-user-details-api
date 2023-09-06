08cfe4a1d7437f72afea41c609922e97af8df4da
KaraboMo <karabo.molemane@adaptit.com>
2023-09-06 16:54:34 +0200

    test rendering of link on UserDetails

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,13 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import Landing from "./components/Landing";
+import UserDetails from "./components/UserDetails";
 import renderer from "react-test-renderer";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders back to Homepage link', () => {
+  render(<UserDetails />);
+  const linkElement = screen.getByText(/Back to Homepage/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
   test('Snapshot test', () => {
     const component = renderer.create(<Landing />);
